fa53c421659865aacb2d8eba1cb8bf32a1cf9127
ccsss <chansol0505@naver.com>
2025-01-09 03:08:27 +0900

    dominant bpm

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 import sys
 import pathlib
 import multiprocessing as mp
-import statistics
 
 import numpy as np
 import librosa
@@ -28,12 +27,101 @@ def get_bpm_from_midi(midi_path):
     """Function to extract BPM information from midi file"""
     mid = mido.MidiFile(midi_path)
     time_signature = (4, 4)  # default time signature
-    for track in mid.tracks:
-        for msg in track:
-            if msg.type == "time_signature":
-                time_signature = (msg.numerator, msg.denominator)
+
+    if mid.type == 0:
+        for track in mid.tracks:
+            for msg in track:
+                if msg.type == "time_signature":
+                    time_signature = (msg.numerator, msg.denominator)
+                elif msg.type == "set_tempo":
+                    return mido.tempo2bpm(msg.tempo, time_signature=time_signature)
+    elif mid.type == 1:
+        item_num = 0
+        maximum_item_num = float("-inf")
+        dominant_tempo = 120
+        for i, msg in enumerate(mido.merge_tracks(mid.tracks)):
+            if msg.type == "note_on" or msg.type == "note_off" or msg.type == "lyrics":
+                item_num += 1
             elif msg.type == "set_tempo":
-                return mido.tempo2bpm(msg.tempo, time_signature=time_signature)
+                tempo = round(mido.tempo2bpm(msg.tempo, time_signature=time_signature))
+                if maximum_item_num < item_num:
+                    dominant_tempo = tempo
+                    maximum_item_num = item_num
+                item_num = 0
+        return dominant_tempo
+
+
+def print_track(
+    track, mid_file, print_bound_per_track=float("inf"), print_dominant_tempo=False
+):
+    """print track"""
+    # default setting
+    time_signature = (4, 4)
+    tempo = 120
+    lyric_encode = "utf-8"
+
+    total_time = 0
+    total_time2 = 0
+    item_num = 0
+    for i, msg in enumerate(track):
+        total_time += mido.tick2second(
+            msg.time, ticks_per_beat=mid_file.ticks_per_beat, tempo=tempo
+        )
+        total_time2 += msg.time
+        if i > print_bound_per_track:
+            continue
+        if msg.type == "note_on":
+            item_num += 1
+            if not print_dominant_tempo:
+                print(
+                    f"{i:4} ┌note on ┐ {pretty_midi.note_number_to_name(msg.note)} ({msg})",
+                )
+        elif msg.type == "note_off":
+            item_num += 1
+            if not print_dominant_tempo:
+                print(
+                    f"{i:4} └note off┘ {pretty_midi.note_number_to_name(msg.note)} ({msg})",
+                )
+        elif msg.type == "end_of_track":
+            print(f"{i:4} [End of Track] (time={msg.time})")
+        elif msg.type == "lyrics":
+            item_num += 1
+            if not print_dominant_tempo:
+                try:
+                    print(
+                        f"{i:4} │ lyrics │ {msg.bin()[3:].decode(lyric_encode)} (time={msg.time})",
+                    )
+                except UnicodeDecodeError:
+                    lyric_encode = "euc-kr"
+        elif msg.type == "channel_prefix":
+            print(f"{i:4} [Channel Prefix] channel={msg.channel} (time={msg.time})")
+        elif msg.type == "track_name":
+            pass  # Track information was already printed
+            # print("[Track name]", msg, msg.bin()[3:].decode(lyric_encode))
+        elif msg.type == "instrument_name":
+            print(f"{i:4} [Instrument Name] {msg.name} (time={msg.time})")
+        elif msg.type == "smpte_offset":
+            print(f"{i:4} [SMPTE] {msg}")
+        elif msg.type == "key_signature":
+            print(f"{i:4} [Key Signature] {msg.key} (time={msg.time})")
+        elif msg.type == "time_signature":
+            print(
+                f"{i:4} [Time Signature] {msg.numerator}/{msg.denominator} "
+                + f"(clocks_per_click={msg.clocks_per_click}, "
+                + f"notated_32nd_notes_per_beat={msg.notated_32nd_notes_per_beat}, "
+                + f"time={msg.time})",
+            )
+            time_signature = (msg.numerator, msg.denominator)
+        elif msg.type == "set_tempo":
+            print(f"item num: {item_num}")
+            tempo = round(mido.tempo2bpm(msg.tempo, time_signature=time_signature))
+            print(f"{i:4} [Tempo] BPM={tempo} (time={msg.time})")
+            item_num = 0
+        else:
+            print(i, msg)
+    print("(test)total ticks", total_time)
+    print("(test)total ticks2", total_time2)
+    print("lyric encode:", lyric_encode)
 
 
 def analysis_midi(midi_path, print_bound_per_track=float("inf")):
@@ -41,7 +129,6 @@ def analysis_midi(midi_path, print_bound_per_track=float("inf")):
     ref: https://mido.readthedocs.io/en/stable/files/midi.html
     """
     sys.stdout.reconfigure(encoding="utf-8")  # printing encoding
-    lyric_encode = "utf-8"  # default lyric encoding
 
     # print header information of midi file
     mid = mido.MidiFile(midi_path)
@@ -50,73 +137,9 @@ def analysis_midi(midi_path, print_bound_per_track=float("inf")):
     print("ticks per beat:", mid.ticks_per_beat)
     print("total duration:", mid.length)
 
-    # default setting
-    time_signature = (4, 4)
-    tempo = 120
-
     for i, track in enumerate(mid.tracks):
         print(f"\nTrack {i}: {track.name}\n")
-        total_time = 0
-        total_time2 = 0
-        for j, msg in enumerate(track):
-            total_time += mido.tick2second(
-                msg.time, ticks_per_beat=mid.ticks_per_beat, tempo=tempo
-            )
-            total_time2 += msg.time
-            if j > print_bound_per_track:
-                continue
-            if msg.type == "note_on":
-                print(
-                    f"{j:4} ┌note on ┐ {pretty_midi.note_number_to_name(msg.note)} ({msg})",
-                )
-            elif msg.type == "note_off":
-                print(
-                    f"{j:4} └note off┘ {pretty_midi.note_number_to_name(msg.note)} ({msg})",
-                )
-            elif msg.type == "end_of_track":
-                print(f"{j:4} [End of Track] (time={msg.time})")
-            elif msg.type == "lyrics":
-                try:
-                    print(
-                        f"{j:4} │ lyrics │ {msg.bin()[3:].decode(lyric_encode)} (time={msg.time})",
-                    )
-                except UnicodeDecodeError:
-                    lyric_encode = "euc-kr"
-            elif msg.type == "channel_prefix":
-                print(f"{j:4} [Channel Prefix] channel={msg.channel} (time={msg.time})")
-            elif msg.type == "track_name":
-                pass  # Track information was already printed
-                # print("[Track name]", msg, msg.bin()[3:].decode(lyric_encode))
-            elif msg.type == "instrument_name":
-                print(f"{j:4} [Instrument Name] {msg.name} (time={msg.time})")
-            elif msg.type == "smpte_offset":
-                print(f"{j:4} [SMPTE] {msg}")
-            elif msg.type == "key_signature":
-                print(f"{j:4} [Key Signature] {msg.key} (time={msg.time})")
-            elif msg.type == "time_signature":
-                print(
-                    f"{j:4} [Time Signature] {msg.numerator}/{msg.denominator} "
-                    + f"(clocks_per_click={msg.clocks_per_click}, "
-                    + f"notated_32nd_notes_per_beat={msg.notated_32nd_notes_per_beat}, "
-                    + f"time={msg.time})",
-                )
-                time_signature = (msg.numerator, msg.denominator)
-            elif msg.type == "set_tempo":
-                tempo = round(mido.tempo2bpm(msg.tempo, time_signature=time_signature))
-                print(f"{j:4} [Tempo] BPM={tempo} (time={msg.time})")
-            else:
-                print(
-                    j,
-                    msg,
-                    mid.ticks_per_beat,
-                    tempo,
-                    mido.tick2second(
-                        msg.time, ticks_per_beat=mid.ticks_per_beat, tempo=tempo
-                    ),
-                )
-        print("(test)total ticks", total_time)
-        print("(test)total ticks2", total_time2)
-    print("lyric encode:", lyric_encode)
+        print_track(track, mid, print_bound_per_track)
 
 
 def test_bpm_estimator_librosa(audio_path):
@@ -192,6 +215,15 @@ def statistics_estimated_bpm_error(path_obj):
     print(np.mean(error_array[:, 3]), np.std(error_array[:, 3]))
 
 
+def test_convert_midi_format_1_to_0(midi_path):
+    """test
+    ref: https://stackoverflow.com/questions/55431137/
+    how-to-convert-midi-type-1-files-to-midi-type-0-in-python-or-command-line"""
+    mid = mido.MidiFile(midi_path)
+    mido.merge_tracks(mid.tracks)
+    print_track(mido.merge_tracks(mid.tracks), mid, print_dominant_tempo=True)
+
+
 if __name__ == "__main__":
     samples = [
         {
@@ -223,15 +255,29 @@ if __name__ == "__main__":
     #         + f' {get_bpm_from_midi(sample["mid"]):.2f}'
     #     )
 
+    # print(bpm_estimator_librosa(samples[2]["wav"])[0])
+
     # analysis_midi(samples[0]["mid"], print_bound_per_track=25)
     # analysis_midi(samples[2]["mid"], print_bound_per_track=40)
+    # analysis_midi(samples[3]["mid"], print_bound_per_track=20)
     # analysis_midi(samples[0]["mid"])
     # analysis_midi(samples[1]["mid"])
     # analysis_midi(samples[2]["mid"])
     # analysis_midi(samples[3]["mid"])
 
-    data_path = pathlib.Path("d:/dataset/004.다화자 가창 데이터")
-    data_path = pathlib.Path("d:/dataset/177.다음색 가이드보컬 데이터")
-    data_path = pathlib.Path("dataset/SINGER_16")
+    # print()
+
+    # test_convert_midi_format_1_to_0(samples[2]["mid"])
     data_path = pathlib.Path("dataset/가창자_s02")
-    statistics_estimated_bpm_error(data_path)
+    for i, mid_path in enumerate(data_path.rglob("*.mid")):
+        if i == 4:
+            test_convert_midi_format_1_to_0(mid_path)
+            print()
+            print(get_bpm_from_midi(mid_path))
+            print(mid_path)
+
+    # data_path = pathlib.Path("d:/dataset/004.다화자 가창 데이터")
+    # data_path = pathlib.Path("d:/dataset/177.다음색 가이드보컬 데이터")
+    # data_path = pathlib.Path("dataset/SINGER_16")
+    # data_path = pathlib.Path("dataset/가창자_s02")
+    # statistics_estimated_bpm_error(data_path)
