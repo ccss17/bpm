e3e9b2f84886c99337fabc7ea600272b6c4eb435
ccsss <chansol0505@naver.com>
2025-01-08 23:13:13 +0900

    analysis midi file

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 """Module for BPM estimating"""
 
+import sys
+
 import librosa
 import pretty_midi
 import mido
@@ -12,16 +14,16 @@ def bpm_estimator_librosa(audio_path):
     return tempo
 
 
-def bpm_estimator_pretty_midi(mid_path):
+def bpm_estimator_pretty_midi(midi_path):
     """Function to estimate BPM from mid file by pretty_midi"""
-    midi_data = pretty_midi.PrettyMIDI(mid_path)
+    midi_data = pretty_midi.PrettyMIDI(midi_path)
     return midi_data.estimate_tempo()
 
 
-def get_bpm_from_midi(mid_path):
+def get_bpm_from_midi(midi_path):
     """Function to extract BPM information from midi file"""
-    mid = mido.MidiFile(mid_path)
-    time_signature = (4, 4)
+    mid = mido.MidiFile(midi_path)
+    time_signature = (4, 4)  # default time signature
     for track in mid.tracks:
         for msg in track:
             if msg.type == "time_signature":
@@ -30,36 +32,26 @@ def get_bpm_from_midi(mid_path):
                 return mido.tempo2bpm(msg.tempo, time_signature=time_signature)
 
 
-def test_bpm_estimator_librosa(audio_path):
-    """Test bpm_estimator_librosa"""
-    print(round(bpm_estimator_librosa(audio_path)[0]))
-
-
-def test_bpm_estimator_pretty_midi(mid_path):
-    """Test bpm_estimator_pretty_midi"""
-    print(round(bpm_estimator_pretty_midi(mid_path)))
-
-
-def test_get_bpm_from_midi(mid_path):
-    """Test get_bpm_from_midi"""
-    print(round(get_bpm_from_midi(mid_path)))
-
-
-def test_mido(mid_path, lyric_encode, print_bound=float("inf")):
-    """Test mido
+def analysis_midi(midi_path, print_bound_per_track=float("inf")):
+    """Function to analysis mid file
     ref: https://mido.readthedocs.io/en/stable/files/midi.html
     """
-    mid = mido.MidiFile(mid_path)
-    print("[미디 파일 헤더]")
+    sys.stdout.reconfigure(encoding="utf-8")  # printing encoding
+    lyric_encode = "utf-8"  # default lyric encoding
+
+    # print header information of midi file
+    mid = mido.MidiFile(midi_path)
+    print("[MIDI File Header]")
     print("mid file type:", mid.type)
     print("ticks per beat:", mid.ticks_per_beat)
     print("total duration:", mid.length)
+
+    # default setting
     time_signature = (4, 4)
     tempo = 120
+
     for i, track in enumerate(mid.tracks):
-        print()
-        print(f"Track {i}: {track.name}")
-        print()
+        print(f"\nTrack {i}: {track.name}\n")
         total_time = 0
         total_time2 = 0
         for j, msg in enumerate(track):
@@ -67,33 +59,47 @@ def test_mido(mid_path, lyric_encode, print_bound=float("inf")):
                 msg.time, ticks_per_beat=mid.ticks_per_beat, tempo=tempo
             )
             total_time2 += msg.time
-            if j > print_bound:
+            if j > print_bound_per_track:
                 continue
             if msg.type == "note_on":
-                print("┌노트 시작┐", msg)
+                print(
+                    f"┌note on ┐ {pretty_midi.note_number_to_name(msg.note)} ({msg})",
+                )
             elif msg.type == "note_off":
-                print("└노트 끝┘", msg)
+                print(
+                    f"└note off┘ {pretty_midi.note_number_to_name(msg.note)} ({msg})",
+                )
+            elif msg.type == "end_of_track":
+                print(f"[End of Track] (time={msg.time})")
             elif msg.type == "lyrics":
-                print(f"  [가사] {msg.bin()[3:].decode(lyric_encode)} time={msg.time}")
+                try:
+                    print(
+                        f"│ lyrics │ {msg.bin()[3:].decode(lyric_encode)} (time={msg.time})",
+                    )
+                except UnicodeDecodeError:
+                    lyric_encode = "euc-kr"
+            elif msg.type == "channel_prefix":
+                print(f"[Channel Prefix] channel={msg.channel} (time={msg.time})")
             elif msg.type == "track_name":
-                print("[트랙 이름]", msg)
+                pass  # Track information was already printed
+                # print("[Track name]", msg, msg.bin()[3:].decode(lyric_encode))
             elif msg.type == "instrument_name":
-                print(f"[악기 이름] {msg.name} time={msg.time}")
+                print(f"[Instrument Name] {msg.name} (time={msg.time})")
             elif msg.type == "smpte_offset":
                 print(f"[SMPTE] {msg}")
             elif msg.type == "key_signature":
-                print(f"[조표] {msg.key} time={msg.time}")
+                print(f"[Key Signature] {msg.key} (time={msg.time})")
             elif msg.type == "time_signature":
                 print(
-                    f"[박자표] {msg.numerator}/{msg.denominator} 박자, "
-                    + f"clocks_per_click={msg.clocks_per_click}, "
+                    f"[Time Signature] {msg.numerator}/{msg.denominator} "
+                    + f"(clocks_per_click={msg.clocks_per_click}, "
                     + f"notated_32nd_notes_per_beat={msg.notated_32nd_notes_per_beat}, "
-                    + f"time={msg.time}"
+                    + f"time={msg.time})"
                 )
                 time_signature = (msg.numerator, msg.denominator)
             elif msg.type == "set_tempo":
                 tempo = round(mido.tempo2bpm(msg.tempo, time_signature=time_signature))
-                print(f"[템포] {msg.tempo}(BPM={tempo}) time={msg.time}")
+                print(f"[Tempo] BPM={tempo} (time={msg.time})")
             else:
                 print(
                     msg,
@@ -103,8 +109,24 @@ def test_mido(mid_path, lyric_encode, print_bound=float("inf")):
                         msg.time, ticks_per_beat=mid.ticks_per_beat, tempo=tempo
                     ),
                 )
-        print("total_time", total_time)
-        print("total_time2", total_time2)
+        print("(test)total ticks", total_time)
+        print("(test)total ticks2", total_time2)
+    print("lyric encode:", lyric_encode)
+
+
+def test_bpm_estimator_librosa(audio_path):
+    """Test bpm_estimator_librosa"""
+    print(round(bpm_estimator_librosa(audio_path)[0]))
+
+
+def test_bpm_estimator_pretty_midi(midi_path):
+    """Test bpm_estimator_pretty_midi"""
+    print(round(bpm_estimator_pretty_midi(midi_path)))
+
+
+def test_get_bpm_from_midi(midi_path):
+    """Test get_bpm_from_midi"""
+    print(round(get_bpm_from_midi(midi_path)))
 
 
 if __name__ == "__main__":
@@ -138,5 +160,9 @@ if __name__ == "__main__":
     #         + f' {get_bpm_from_midi(sample["mid"]):.2f}'
     #     )
 
-    # test_mido(samples[0]["mid"], lyric_encode="utf-8", print_bound=25)
-    test_mido(samples[2]["mid"], lyric_encode="euc-kr", print_bound=20)
+    # analysis_midi(samples[0]["mid"], print_bound_per_track=25)
+    analysis_midi(samples[2]["mid"], print_bound_per_track=20)
+    # analysis_midi(samples[0]["mid"])
+    # analysis_midi(samples[1]["mid"])
+    # analysis_midi(samples[2]["mid"])
+    # analysis_midi(samples[3]["mid"])
