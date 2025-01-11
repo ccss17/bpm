dbddbe4f0f51bc78b3bf7c51522ca59a43de00ae
ccsss <chansol0505@naver.com>
2025-01-11 18:24:26 +0900

    bpmlib

--- bpmlib.py
+++ bpmlib.py
@@ -1,13 +1,22 @@
 """Module for BPM estimating"""
 
+import os
 import sys
 import multiprocessing as mp
 import random
+from collections import defaultdict
 
 import numpy as np
 import librosa
 import pretty_midi
 import mido
+from mido import MidiFile
+from pydub import AudioSegment
+from pydub.generators import Sine
+
+DEFAULT_BPM = 120
+DEFAULT_TEMPO = 500000
+DEFAULT_TIME_SIGNATURE = (4, 4)
 
 
 def bpm_estimator_librosa(audio_path):
@@ -23,41 +32,51 @@ def bpm_estimator_pretty_midi(midi_path):
     return midi_data.estimate_tempo()
 
 
+def _bpm_from_midi_format_0(mid_obj):
+    time_signature = DEFAULT_TIME_SIGNATURE  # default time signature
+    for msg in mid_obj.tracks[0]:
+        if msg.type == "time_signature":
+            time_signature = (msg.numerator, msg.denominator)
+        elif msg.type == "set_tempo":
+            return mido.tempo2bpm(msg.tempo, time_signature=time_signature)
+
+
+def _bpm_from_midi_format_1(mid_obj):
+    bpm = DEFAULT_BPM
+    time_signature = DEFAULT_TIME_SIGNATURE
+    # convert midi format 1 to 0
+    merged_track = mido.merge_tracks(mid_obj.tracks)
+    lyric_note_num = 0
+    total_lyric_note_num = 0
+    tempo_mean_numerator = 0
+    first_tempo = True
+    for msg in merged_track:
+        if msg.type == "note_on" or msg.type == "note_off" or msg.type == "lyrics":
+            lyric_note_num += 1
+        elif msg.type == "set_tempo":
+            if first_tempo:
+                first_tempo = False
+            else:
+                tempo_mean_numerator += bpm * lyric_note_num
+                total_lyric_note_num += lyric_note_num
+            bpm = round(mido.tempo2bpm(msg.tempo, time_signature=time_signature))
+            lyric_note_num = 0
+        elif msg.type == "time_signature":
+            time_signature = (msg.numerator, msg.denominator)
+        elif msg.type == "end_of_track":
+            tempo_mean_numerator += bpm * lyric_note_num
+            total_lyric_note_num += lyric_note_num
+    return tempo_mean_numerator / total_lyric_note_num
+
+
 def get_bpm_from_midi(midi_path):
     """Function to extract BPM information from midi file"""
     mid = mido.MidiFile(midi_path)
-    time_signature = (4, 4)  # default time signature
 
     if mid.type == 0:
-        for msg in mid.tracks[0]:
-            if msg.type == "time_signature":
-                time_signature = (msg.numerator, msg.denominator)
-            elif msg.type == "set_tempo":
-                return mido.tempo2bpm(msg.tempo, time_signature=time_signature)
+        return _bpm_from_midi_format_0(mid)
     elif mid.type == 1:
-        tempo = 120  # default tempo
-        merged_track = mido.merge_tracks(mid.tracks)
-        lyric_note_num = 0
-        total_lyric_note_num = 0
-        tempo_mean_numerator = 0
-        first_tempo = True
-        for msg in merged_track:
-            if msg.type == "note_on" or msg.type == "note_off" or msg.type == "lyrics":
-                lyric_note_num += 1
-            elif msg.type == "set_tempo":
-                if first_tempo:
-                    first_tempo = False
-                else:
-                    tempo_mean_numerator += tempo * lyric_note_num
-                    total_lyric_note_num += lyric_note_num
-                tempo = round(mido.tempo2bpm(msg.tempo, time_signature=time_signature))
-                lyric_note_num = 0
-            elif msg.type == "time_signature":
-                time_signature = (msg.numerator, msg.denominator)
-            elif msg.type == "end_of_track":
-                tempo_mean_numerator += tempo * lyric_note_num
-                total_lyric_note_num += lyric_note_num
-        return tempo_mean_numerator / total_lyric_note_num
+        return _bpm_from_midi_format_1(mid)
     elif mid.type == 2:
         raise NotImplementedError
     else:
@@ -65,7 +84,7 @@ def get_bpm_from_midi(midi_path):
 
 
 def print_track(
-    track, mid_file, print_bound_per_track=float("inf"), print_dominant_tempo=False
+    track, mid_file, print_bound_per_track=float("inf"), blind_note_lyrics=False
 ):
     """print track"""
 
@@ -73,9 +92,9 @@ def print_track(
         print(" " * 8 + f"Total item num: {lyric_note_num}")
 
     # default setting
-    time_signature = (4, 4)
-    bpm = 120
-    tempo = 500000
+    time_signature = DEFAULT_TIME_SIGNATURE
+    bpm = DEFAULT_BPM
+    tempo = DEFAULT_TEMPO
     lyric_encode = "utf-8"
 
     total_time = 0
@@ -83,14 +102,14 @@ def print_track(
     first_tempo = True
     for i, msg in enumerate(track):
         if i > print_bound_per_track:
-            continue
+            break
         if msg.type == "note_on":
             lyric_note_num += 1
             time = mido.tick2second(
                 msg.time, ticks_per_beat=mid_file.ticks_per_beat, tempo=tempo
             )
             total_time += time
-            if not print_dominant_tempo:
+            if not blind_note_lyrics:
                 print(
                     f"{i:4} ┌note on ┐ {pretty_midi.note_number_to_name(msg.note)} {time:.2f}/{total_time:.2f} ({msg})",
                 )
@@ -100,39 +119,42 @@ def print_track(
                 msg.time, ticks_per_beat=mid_file.ticks_per_beat, tempo=tempo
             )
             total_time += time
-            if not print_dominant_tempo:
+            if not blind_note_lyrics:
                 print(
                     f"{i:4} └note off┘ {pretty_midi.note_number_to_name(msg.note)} {time:.2f}/{total_time:.2f} ({msg})",
                 )
-        elif msg.type == "set_tempo":
-            if not first_tempo:
-                print_lyric_note_num(lyric_note_num)
-            else:
-                first_tempo = False
-            bpm = round(mido.tempo2bpm(msg.tempo, time_signature=time_signature))
-            print(f"{i:4} [Tempo] {msg.tempo} BPM={bpm} (time={msg.time})")
-            lyric_note_num = 0
-        elif msg.type == "end_of_track":
-            print_lyric_note_num(lyric_note_num)
-            print(f"{i:4} [End of Track] (time={msg.time})")
         elif msg.type == "lyrics":
             lyric_note_num += 1
             time = mido.tick2second(
                 msg.time, ticks_per_beat=mid_file.ticks_per_beat, tempo=tempo
             )
             total_time += time
-            if not print_dominant_tempo:
+            if not blind_note_lyrics:
                 try:
                     print(
-                        f"{i:4} │ lyrics │ {msg.bin()[3:].decode(lyric_encode)} {time:.2f}/{total_time:.2f}",
+                        f"{i:4} │ lyrics │ {msg.bin()[3:].decode(lyric_encode)} {time:.2f}/{total_time:.2f} (time={msg.time})",
                     )
                 except UnicodeDecodeError:
                     lyric_encode = "euc-kr"
+        elif msg.type == "set_tempo":
+            if not first_tempo:
+                print_lyric_note_num(lyric_note_num)
+            else:
+                first_tempo = False
+            tempo = msg.tempo
+            bpm = round(mido.tempo2bpm(msg.tempo, time_signature=time_signature))
+            print(f"{i:4} [Tempo] {msg.tempo} BPM={bpm} (time={msg.time})")
+            lyric_note_num = 0
+        elif msg.type == "end_of_track":
+            total_time += mido.tick2second(
+                msg.time, ticks_per_beat=mid_file.ticks_per_beat, tempo=tempo
+            )
+            print_lyric_note_num(lyric_note_num)
+            print(f"{i:4} [End of Track] (time={msg.time})")
         elif msg.type == "channel_prefix":
             print(f"{i:4} [Channel Prefix] channel={msg.channel} (time={msg.time})")
         elif msg.type == "track_name":
             print(f"{i:4} [Track name] {msg} {msg.bin()[3:].decode(lyric_encode)}")
-            # pass  # Track information was already printed
         elif msg.type == "instrument_name":
             print(f"{i:4} [Instrument Name] {msg.name} (time={msg.time})")
         elif msg.type == "smpte_offset":
@@ -149,14 +171,16 @@ def print_track(
             time_signature = (msg.numerator, msg.denominator)
         else:
             print(i, msg)
-    print("total time", total_time)
-    print("lyric encode:", lyric_encode)
+
+    if print_bound_per_track == float("inf"):
+        print("total time", total_time)
+        print("lyric encode:", lyric_encode)
 
 
 def analysis_midi(
     midi_path,
     print_bound_per_track=float("inf"),
-    print_dominant_tempo=False,
+    blind_note_lyrics=False,
     convert_1_to_0=False,
 ):
     """Function to analysis mid file
@@ -164,6 +188,7 @@ def analysis_midi(
     """
     sys.stdout.reconfigure(encoding="utf-8")  # printing encoding
 
+    print(f"ANALYSIS MIDI FILE: {midi_path}")
     # print header information of midi file
     mid = mido.MidiFile(midi_path)
     print("[MIDI File Header]")
@@ -176,7 +201,7 @@ def analysis_midi(
 
     for i, track in enumerate(mid.tracks):
         print(f"\nTrack {i}: {track.name}\n")
-        print_track(track, mid, print_bound_per_track, print_dominant_tempo)
+        print_track(track, mid, print_bound_per_track, blind_note_lyrics)
 
 
 def estimated_bpm_error(audio_path, midi_path):
@@ -192,23 +217,29 @@ def estimated_bpm_error(audio_path, midi_path):
     corrected_underestimated_bpm_error_4 = abs(estimated_bpm * 4 - bpm_from_midi_file)
     corrected_underestimated_bpm_error_8 = abs(estimated_bpm * 8 - bpm_from_midi_file)
 
+    selected_error = np.argmin(
+        [
+            corrected_overestimated_bpm_error_8,
+            corrected_overestimated_bpm_error_4,
+            corrected_overestimated_bpm_error_2,
+            error,
+            corrected_underestimated_bpm_error_2,
+            corrected_underestimated_bpm_error_4,
+            corrected_underestimated_bpm_error_8,
+        ]
+    )
+
     corrected_error_2 = min(
         error, corrected_overestimated_bpm_error_2, corrected_underestimated_bpm_error_2
     )
     corrected_error_4 = min(
-        error,
+        corrected_error_2,
         corrected_overestimated_bpm_error_4,
-        corrected_overestimated_bpm_error_2,
-        corrected_underestimated_bpm_error_2,
         corrected_underestimated_bpm_error_4,
     )
     corrected_error_8 = min(
-        error,
+        corrected_error_4,
         corrected_overestimated_bpm_error_8,
-        corrected_overestimated_bpm_error_4,
-        corrected_overestimated_bpm_error_2,
-        corrected_underestimated_bpm_error_2,
-        corrected_underestimated_bpm_error_4,
         corrected_underestimated_bpm_error_8,
     )
     return (
@@ -216,6 +247,7 @@ def estimated_bpm_error(audio_path, midi_path):
         corrected_error_2,
         corrected_error_4,
         corrected_error_8,
+        selected_error,
     )
 
 
@@ -243,3 +275,60 @@ def statistics_estimated_bpm_error(path_obj, sample_num=None):
     print(
         f"error(8); mean/std: {np.mean(error_array[:, 3]):5.2f}, {np.std(error_array[:, 3]):5.2f}"
     )
+
+    selected_error_frequencies = np.bincount(error_array[:, 4].astype(np.int64))
+    selected_error_frequencies = np.pad(
+        selected_error_frequencies,
+        (0, 7 - selected_error_frequencies.shape[0]),
+        "constant",
+        constant_values=(0, 0),
+    )
+    print("selected error:")
+    print(f"  error(/8): {selected_error_frequencies[0]}")
+    print(f"  error(/4): {selected_error_frequencies[1]}")
+    print(f"  error(/2): {selected_error_frequencies[2]}")
+    print(f"  error(0) : {selected_error_frequencies[3]}")
+    print(f"  error(*2): {selected_error_frequencies[4]}")
+    print(f"  error(*4): {selected_error_frequencies[5]}")
+    print(f"  error(*8): {selected_error_frequencies[6]}")
+
+
+def midi2wav(midi_path, wav_path, bpm):
+    """Function to convert midi to wav
+    ref: https://gist.github.com/jiaaro/339df443b005e12d6c2a"""
+
+    def note_to_freq(note, concert_A=440.0):
+        """
+        from wikipedia: http://en.wikipedia.org/wiki/MIDI_Tuning_Standard#Frequency_values
+        """
+        return (2.0 ** ((note - 69) / 12.0)) * concert_A
+
+    mid = MidiFile(midi_path)
+    output = AudioSegment.silent(mid.length * 1000.0)
+
+    def ticks_to_ms(ticks):
+        tick_ms = (60000.0 / bpm) / mid.ticks_per_beat
+        return ticks * tick_ms
+
+    for track in mid.tracks:
+        # position of rendering in ms
+        current_pos = 0.0
+        current_notes = defaultdict(dict)
+
+        for msg in track:
+            current_pos += ticks_to_ms(msg.time)
+            if msg.type == "note_on":
+                current_notes[msg.channel][msg.note] = (current_pos, msg)
+            if msg.type == "note_off":
+                start_pos, start_msg = current_notes[msg.channel].pop(msg.note)
+                duration = current_pos - start_pos
+                signal_generator = Sine(note_to_freq(msg.note))
+                rendered = (
+                    signal_generator.to_audio_segment(
+                        duration=duration - 50, volume=-20
+                    )
+                    .fade_out(100)
+                    .fade_in(30)
+                )
+                output = output.overlay(rendered, start_pos)
+    output.export(wav_path, format="wav")
--- test.py
+++ test.py
@@ -4,32 +4,102 @@ import pathlib
 
 import mido
 
-import bpm
+import bpmlib
 
 
 def test_bpm_estimator_librosa(audio_path):
     """Test bpm_estimator_librosa"""
-    print(round(bpm.bpm_estimator_librosa(audio_path)[0]))
+    print(round(bpmlib.bpm_estimator_librosa(audio_path)[0]))
 
 
 def test_bpm_estimator_pretty_midi(midi_path):
     """Test bpm_estimator_pretty_midi"""
-    print(round(bpm.bpm_estimator_pretty_midi(midi_path)))
+    print(round(bpmlib.bpm_estimator_pretty_midi(midi_path)))
 
 
 def test_get_bpm_from_midi(midi_path):
     """Test get_bpm_from_midi"""
-    print(round(bpm.get_bpm_from_midi(midi_path)))
+    print(round(bpmlib.get_bpm_from_midi(midi_path)))
 
 
-def test_convert_midi_format_1_to_0(midi_path, print_dominant_tempo=True):
+def test_convert_midi_format_1_to_0(midi_path, blind_note_lyrics=True):
     """test
     ref: https://stackoverflow.com/questions/55431137/
     how-to-convert-midi-type-1-files-to-midi-type-0-in-python-or-command-line"""
     mid = mido.MidiFile(midi_path)
     merged_track = mido.merge_tracks(mid.tracks)
     print(f"\nTrack 0: {merged_track.name}\n")
-    bpm.print_track(merged_track, mid, print_dominant_tempo=print_dominant_tempo)
+    bpmlib.print_track(merged_track, mid, blind_note_lyrics=blind_note_lyrics)
+
+
+def test_create_sample_midi1(midi_path):
+    """test_sample_midi"""
+    mid = mido.MidiFile()
+    mid.ticks_per_beat = 2
+    track = mido.MidiTrack()
+    mid.tracks.append(track)
+
+    track.append(mido.MetaMessage("set_tempo", tempo=mido.bpm2tempo(60), time=0))
+    track.append(mido.Message("note_on", note=64, velocity=64, time=4))
+    track.append(mido.Message("note_off", note=64, velocity=127, time=8))
+    track.append(mido.Message("note_on", note=74, velocity=64, time=4))
+    track.append(mido.Message("note_off", note=74, velocity=127, time=8))
+    track.append(mido.MetaMessage("end_of_track", time=8))
+
+    mid.save(midi_path)
+
+
+def test_create_sample_midi2(midi_path):
+    """test_sample_midi"""
+    mid = mido.MidiFile()
+    mid.ticks_per_beat = 2
+    track = mido.MidiTrack()
+    mid.tracks.append(track)
+
+    track.append(mido.MetaMessage("set_tempo", tempo=mido.bpm2tempo(60), time=0))
+    track.append(mido.Message("note_on", note=64, velocity=64, time=4))
+    track.append(mido.MetaMessage("lyrics", text="a", time=4))
+    track.append(mido.Message("note_off", note=64, velocity=127, time=8))
+    track.append(mido.MetaMessage("lyrics", text="b", time=4))
+    track.append(mido.Message("note_on", note=74, velocity=64, time=4))
+    track.append(mido.Message("note_off", note=74, velocity=127, time=8))
+    track.append(mido.MetaMessage("lyrics", text="d", time=4))
+
+    mid.save(midi_path)
+
+
+def test_create_sample_midi3(midi_path):
+    """test_sample_midi"""
+    mid = mido.MidiFile()
+    mid.ticks_per_beat = 2
+    track = mido.MidiTrack()
+    mid.tracks.append(track)
+
+    track.append(mido.MetaMessage("set_tempo", tempo=mido.bpm2tempo(60), time=0))
+    track.append(mido.Message("note_on", note=64, velocity=64, time=4))
+    track.append(mido.MetaMessage("lyrics", text="a", time=6))
+    track.append(mido.MetaMessage("lyrics", text="b", time=4))
+    track.append(mido.Message("note_off", note=64, velocity=127, time=8))
+
+    mid.save(midi_path)
+
+
+def test_create_sample_midi4(midi_path):
+    """test_sample_midi"""
+    mid = mido.MidiFile()
+    mid.ticks_per_beat = 2
+    track = mido.MidiTrack()
+    mid.tracks.append(track)
+
+    track.append(mido.MetaMessage("set_tempo", tempo=mido.bpm2tempo(60), time=0))
+    track.append(mido.Message("note_on", note=64, velocity=64, time=2))
+    track.append(mido.MetaMessage("lyrics", text="a", time=12))
+    track.append(mido.Message("note_on", note=74, velocity=64, time=2))
+    track.append(mido.Message("note_off", note=64, velocity=127, time=8))
+    track.append(mido.MetaMessage("lyrics", text="b", time=10))
+    track.append(mido.Message("note_off", note=74, velocity=127, time=8))
+
+    mid.save(midi_path)
 
 
 if __name__ == "__main__":
@@ -52,62 +122,104 @@ if __name__ == "__main__":
         },
     ]
 
+    #
+    # BPM ESTIMATOR vs BPM FROM MIDI
+    #
     # test_bpm_estimator_librosa(samples[0]["wav"])
     # test_bpm_estimator_pretty_midi(samples[0]["mid"])
     # test_get_bpm_from_midi(samples[0]["mid"])
 
     # for sample in samples:
     #     print(
-    #         f'{bpm.bpm_estimator_librosa(sample["wav"])[0]:.2f}'
-    #         + f' {bpm.bpm_estimator_pretty_midi(sample["mid"]):.2f}'
-    #         + f' {bpm.get_bpm_from_midi(sample["mid"]):.2f}'
+    #         f'{bpmlib.bpm_estimator_librosa(sample["wav"])[0]:.2f}'
+    #         + f' {bpmlib.bpm_estimator_pretty_midi(sample["mid"]):.2f}'
+    #         + f' {bpmlib.get_bpm_from_midi(sample["mid"]):.2f}'
     #     )
 
-    # bpm.analysis_midi(samples[0]["mid"], print_bound_per_track=15)
-    # bpm.analysis_midi(samples[2]["mid"], print_bound_per_track=40)
-    # bpm.analysis_midi(samples[3]["mid"], print_bound_per_track=20)
-    bpm.analysis_midi(samples[0]["mid"])
-    # bpm.analysis_midi(samples[1]["mid"])
-    # bpm.analysis_midi(samples[2]["mid"])
-    # bpm.analysis_midi(samples[3]["mid"], print_dominant_tempo=True, convert_1_to_0=True)
-    # bpm.analysis_midi(
-    #     samples[3]["mid"], print_dominant_tempo=False, convert_1_to_0=True
+    #
+    # ANALYSIS MIDI FILE
+    #
+    # bpmlib.analysis_midi(samples[0]["mid"], print_bound_per_track=15)
+    # bpmlib.analysis_midi(samples[2]["mid"], print_bound_per_track=40)
+    # bpmlib.analysis_midi(samples[3]["mid"], print_bound_per_track=20)
+    # bpmlib.analysis_midi(samples[0]["mid"])
+    # bpmlib.analysis_midi(samples[1]["mid"])
+    # bpmlib.analysis_midi(samples[2]["mid"])
+
+    #
+    # CONVERT MIDI FORMAT 1 TO 0
+    #
+    # bpmlib.analysis_midi(samples[3]["mid"], blind_note_lyrics=True, convert_1_to_0=True)
+    # bpmlib.analysis_midi(
+    #     samples[3]["mid"], blind_note_lyrics=False, convert_1_to_0=True
     # )
-    # test_get_bpm_from_midi(samples[3]["mid"])
-
-    # x = 520
-    # print(mido.tick2second(2000, x, 508475))
-    # print(mido.tick2second(2267, x, 508475))
-    # print(mido.tick2second(2280, x, 508475))
-    # print(mido.tick2second(2340, x, 508475))
-
-    # test_convert_midi_format_1_to_0(samples[2]["mid"])
     # data_path = pathlib.Path("dataset/가창자_s02")
     # for i, mid_path in enumerate(data_path.rglob("*.mid")):
     #     if i == 4:
-    #         # test_convert_midi_format_1_to_0(mid_path, print_dominant_tempo=False)
-    #         test_convert_midi_format_1_to_0(mid_path, print_dominant_tempo=True)
+    #         test_convert_midi_format_1_to_0(mid_path, blind_note_lyrics=True)
 
-    # sample_num = 34
+    #
+    # CREATE SAMPLE MIDI AND ANALYSIS IT
+    #
+    # test_create_sample_midi1("test_sample1.mid")
+    # bpmlib.analysis_midi("test_sample1.mid")
+    # bpmlib.midi2wav("test_sample1.mid", "test_sample1.wav", 60)
+    # print("-" * 70)
+    # test_create_sample_midi2("test_sample2.mid")
+    # bpmlib.analysis_midi("test_sample2.mid")
+    # bpmlib.midi2wav("test_sample2.mid", "test_sample2.wav", 60)
+    # print("-" * 70)
+    # test_create_sample_midi3("test_sample3.mid")
+    # bpmlib.analysis_midi("test_sample3.mid")
+    # bpmlib.midi2wav("test_sample3.mid", "test_sample3.wav", 60)
+    # print("-" * 70)
+    # test_create_sample_midi4("test_sample4.mid")
+    # bpmlib.analysis_midi("test_sample4.mid")
+    # bpmlib.midi2wav("test_sample4.mid", "test_sample4.wav", 60)
+
+    #
+    # GET STATISTICS of ESTIMATED CORRECTED BPM ERROR
+    #
+    # sample_num = 10
     # data_path = pathlib.Path("dataset/SINGER_16")
-    # bpm.statistics_estimated_bpm_error(data_path, sample_num=sample_num)
+    # bpmlib.statistics_estimated_bpm_error(data_path)
+    # bpmlib.statistics_estimated_bpm_error(data_path, sample_num=sample_num)
+    # print()
     # data_path = pathlib.Path("dataset/가창자_s02")
-    # bpm.statistics_estimated_bpm_error(data_path)
+    # bpmlib.statistics_estimated_bpm_error(data_path)
 
     # data_path = pathlib.Path("d:/dataset/177.다음색 가이드보컬 데이터")
     # print(data_path)
-    # bpm.statistics_estimated_bpm_error(data_path)
+    # bpmlib.statistics_estimated_bpm_error(data_path)
+    # print()
     # data_path = pathlib.Path("d:/dataset/004.다화자 가창 데이터")
     # print(data_path)
-    # bpm.statistics_estimated_bpm_error(data_path)
+    # bpmlib.statistics_estimated_bpm_error(data_path)
     # Output:
     # d:\dataset\177.다음색 가이드보컬 데이터
     # error(0); mean/std: 42.13, 35.27
     # error(2); mean/std:  4.39,  7.71
     # error(4); mean/std:  4.08,  6.77
     # error(8); mean/std:  4.08,  6.77
+    # selected error:
+    #   error(/8): 0
+    #   error(/4): 41
+    #   error(/2): 1998
+    #   error(0) : 1541
+    #   error(*2): 29
+    #   error(*4): 0
+    #   error(*8): 0
+
     # d:\dataset\004.다화자 가창 데이터
     # error(0); mean/std: 38.97, 36.49
     # error(2); mean/std:  6.16,  8.72
     # error(4); mean/std:  5.91,  8.17
     # error(8); mean/std:  5.91,  8.17
+    # selected error:
+    #   error(/8): 0
+    #   error(/4): 78
+    #   error(/2): 1993
+    #   error(0) : 2078
+    #   error(*2): 77
+    #   error(*4): 0
+    #   error(*8): 0
